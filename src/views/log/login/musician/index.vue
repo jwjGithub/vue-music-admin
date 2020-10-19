a75ca2212192d0daccc293f6258c31cbcb908205
parker <jwj1028198397@163.com>
2020-10-19 22:23:30 +0800

    update

--- src/views/log/login/musician/index.vue
+++ src/views/log/login/musician/index.vue
@@ -4,8 +4,14 @@
       <el-form ref="queryForm" label-width="100px" :model="queryForm" :inline="true">
         <div class="query-item">
           <div class="left-query">
-            <el-form-item label="用户ID" prop="userId">
-              <el-input v-model="queryForm.userId" class="w24"></el-input>
+            <el-form-item label="用户账号" prop="username">
+              <el-input v-model="queryForm.username" class="w24"></el-input>
+            </el-form-item>
+            <el-form-item label="用户姓名" prop="realName">
+              <el-input v-model="queryForm.realName" class="w24"></el-input>
+            </el-form-item>
+            <el-form-item label="登录地" prop="cname">
+              <el-input v-model="queryForm.cname" class="w24"></el-input>
             </el-form-item>
             <el-form-item label="IP" prop="ip">
               <el-input v-model="queryForm.ip" class="w24"></el-input>
@@ -40,11 +46,11 @@
             stripe
             style="width: 100%"
           >
-            <el-table-column prop="userId" min-width="150" label="登录用户"></el-table-column>
-            <el-table-column prop="ip" min-width="150" label="登录IP"></el-table-column>
-            <el-table-column prop="username" min-width="150" label="登录人账号"></el-table-column>
             <el-table-column prop="realName" min-width="150" label="登录人姓名"></el-table-column>
-            <el-table-column prop="createdTime" min-width="180" label="创建时间"></el-table-column>
+            <el-table-column prop="username" min-width="150" label="登录人账号"></el-table-column>
+            <el-table-column prop="cname" min-width="150" label="登录地"></el-table-column>
+            <el-table-column prop="ip" min-width="150" label="登录IP"></el-table-column>
+            <el-table-column prop="createdTime" min-width="180" label="登录时间"></el-table-column>
           </el-table>
         </el-col>
       </el-row>
@@ -70,7 +76,9 @@ export default {
       loading: false,
       dataList: [],
       queryForm: {
-        userId: '',
+        cname: null, // 登录地
+        username: null, // 账号
+        realName: null, // 姓名
         ip: '',
         startTime: '', // 开始时间
         endTime: '', // 结束时间
